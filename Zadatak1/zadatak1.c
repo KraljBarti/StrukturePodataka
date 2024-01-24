1e6be3c5c84153d5ede6b0aaa13fc316fe488200
KraljBarti <barti.kujundzic75@gmail.com>
2024-01-24 19:34:25 +0100

    11. zadatak

--- Zadatak1/zadatak1.c
+++ Zadatak1/zadatak1.c
@@ -1,4 +1,4 @@
-﻿/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata
+/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj. koliko ima studenata
 zapisanih u datoteci. Nakon toga potrebno je dinamički alocirati prostor za niz struktura
 studenata (ime, prezime, bodovi) i učitati iz datoteke sve zapise. Na ekran ispisati ime,
 prezime, apsolutni i relativni broj bodova.
@@ -17,7 +17,7 @@ struct Student {
 };
 
 int main() {
-
+	int i;
 	FILE* datoteka;
 	int brojStudenata;
 	double max_br_bodova = 100.0; // Maksimalan broj bodova (u ovom slučaju 100)
@@ -36,16 +36,16 @@ int main() {
 
 	//Dinamička alokacija prostora za niz struktura studenata
 	struct Student* studenti = (struct Student*)malloc(brojStudenata * sizeof(struct Student));
-
+	
 	//Čitanje podataka o studentima iz datoteke
-	for (int i = 0; i < brojStudenata; i++) {
+	for (i = 0; i < brojStudenata; i++) {
 		fscanf(datoteka, "%s %s %d", studenti[i].ime, studenti[i].prezime, &studenti[i].bodovi);
 
 	}
 	//Zatvaranje datoteke
 	fclose(datoteka);
 	//Ispisivanje podataka o studentima i izračun relativnih bodova
-	for (int i = 0; i < brojStudenata; i++) {
+	for (i = 0; i < brojStudenata; i++) {
 		double relativni_br_bodova = ((double)studenti[i].bodovi / max_br_bodova) * 100;
 		printf("Ime: %s, Prezime: %s, Apsolutni bodovi: %d, Relativni bodovi: %.2lf%%\n", studenti[i].ime,studenti[i].prezime,studenti[i].bodovi,relativni_br_bodova);
 
